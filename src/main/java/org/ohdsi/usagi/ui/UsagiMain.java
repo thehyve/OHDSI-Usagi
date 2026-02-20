d968e2c101b57237451b4a60d5a05fc6e2bc797b
Jan Blom <jan.j.blom@gmail.com>
2026-02-20 16:14:29 +0100

    Drop older option for anti-aliased fonts

--- src/main/java/org/ohdsi/usagi/ui/UsagiMain.java
+++ src/main/java/org/ohdsi/usagi/ui/UsagiMain.java
@@ -50,7 +50,6 @@ public class UsagiMain implements ActionListener {
 
 	public static void main(String[] args) {
 		System.setProperty("awt.useSystemAAFontSettings", "on");
-		System.setProperty("swing.aatext", "true");
 		new UsagiMain(true, args);
 	}
 
